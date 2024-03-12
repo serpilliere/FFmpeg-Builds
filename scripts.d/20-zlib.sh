368c76d3f64b8b62946f526c1e0e11e3cf1f7511
Fabrice Desclaux <fabrice.desclaux@cea.fr>
2024-03-12 23:05:50 +0100

    Add zlib

---
+++ scripts.d/20-zlib.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/madler/zlib.git"
+SCRIPT_COMMIT="5c42a230b7b468dff011f444161c0145b5efae59"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --static
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        export CC="${FFBUILD_CROSS_PREFIX}gcc"
+        export AR="${FFBUILD_CROSS_PREFIX}ar"
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    ./configure "${myconf[@]}"
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    echo --enable-zlib
+}
+
+ffbuild_unconfigure() {
+    echo --disable-zlib
+}
