0244ee1317ce3da5ff0024d196b71e90f90a7972
Fabrice Desclaux <fabrice.desclaux@cea.fr>
2024-03-12 21:50:26 +0100

    Rm network

--- variants/defaults-gpl-shared.sh
+++ variants/defaults-gpl-shared.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source "$(dirname "$BASH_SOURCE")"/defaults-gpl.sh
-FF_CONFIGURE+=" --enable-shared --disable-static --disable-opencl --disable-mediafoundation --disable-amf --disable-indevs"
+FF_CONFIGURE+=" --enable-shared --disable-static --disable-opencl --disable-mediafoundation --disable-amf --disable-indevs --disable-network"
